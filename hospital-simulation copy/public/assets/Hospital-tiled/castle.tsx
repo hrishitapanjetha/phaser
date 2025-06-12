03b11bd69b4d030cb6163b54d738abb4f333305b
Hrishita Panjetha <hrishita.panjetha@gmail.com>
2025-06-12 18:22:02 +0530

    changes with  new simulation

--- hospital-simulation copy/public/assets/Hospital-tiled/castle.tsx
+++ hospital-simulation copy/public/assets/Hospital-tiled/castle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="castle" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="castle.png" width="512" height="512"/>
+<tileset version="1.10" tiledversion="1.11.2" name="castle" tilewidth="16" tileheight="16" tilecount="961" columns="31">
+ <image source="../../../../../hospital-simulation-amity/hospital-simulation/public/assets/Hospital-tiled/kenney_platformer-art-extended-tileset/PNG Castle/Spritesheet/sheet.png" width="509" height="509"/>
 </tileset>
--- hospital-simulation copy/public/assets/Hospital-tiled/shop-and-hospital.tsx
+++ hospital-simulation copy/public/assets/Hospital-tiled/shop-and-hospital.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="shop-and-hospital" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="shop-and-hospital.png" width="512" height="512"/>
+<tileset version="1.10" tiledversion="1.11.2" name="shop-and-hospital" tilewidth="16" tileheight="16" tilecount="961" columns="31">
+ <image source="../../../../../hospital-simulation-amity/hospital-simulation/public/assets/Hospital-tiled/Kauzz Free Tiles I/shop-and-hospital.png" width="509" height="509"/>
 </tileset>
